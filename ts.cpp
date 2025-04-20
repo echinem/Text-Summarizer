0a9dd970b2c75d275621c2192316fb1e4642ef0e
echinem <developer.himangi@gmail.com>
2025-04-20 17:47:02 +0530

    Add function to retrieve text from .txt

--- ts.cpp
+++ ts.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+
 using namespace std;
 
 //enter function to parse text
 
-// enter function to extract text from file
+string fileText(string f){
+    ifstream file(f);
+    string text, line;
+    //add error handling here
+    if(!file){
+        cout << "\n\t Failed to open file..." << endl;
+        return "\n";
+    }
+    while(getline(file, line)){
+        text += line + " ";
+    }
+    file.close();
+    return text;
+}
 
 int menu(){
     int choice;
@@ -17,18 +33,35 @@ int menu(){
 int main(){
     cout << "Initializing my project!";
     int choice;
+    string text, next;
     while(choice!=3){
         choice = menu();
         if(choice == 1){
+            string filename;
             cout << "\n\tEnter TEXT file location: ";
+            cin >> filename;
+            text = fileText(filename);
         }else if(choice == 2){
-            cout << "\n\tEnter text: ";
+            cout << "\n\t(If you're pasting, please press Ctrl+Shift+V)\n\tEnter text: ";
+            cin.ignore();
+            getline(cin, text);
         }else if(choice == 3){
             cout << "\n\n\n=== EXITING ===\n\n\n";
             break;
         }else{
             cout << "\nPlease enter 1 / 2 / 3\n\n";
         }
+        cout << endl << endl << text << endl << endl;
+        
+        //function call to further: processText(text);
+        
+        
+        /*
+        To find summary of another text
+        cout << "\n\n\tPress 3 to EXIT\n\tPress any other key to proceed to MENU";
+        cin >> choice;
+        */
+
     }
     return 0;
 }
