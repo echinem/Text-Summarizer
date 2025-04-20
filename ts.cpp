ae9b8fb9f3c4f4bfdc9a14cbe046d668b025a765
echinem <developer.himangi@gmail.com>
2025-04-20 12:41:34 +0530

    Add menu

--- ts.cpp
+++ ts.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 using namespace std;
 
+//enter function to parse text
+
+// enter function to extract text from file
+
+int menu(){
+    int choice;
+    cout << "\n\n===== T E X T\tS U M M A R I Z E R=====\n\n";
+    cout << "\t1. Choose .txt file\n\t2. Enter text\n\t3. Exit\n\n";
+    cout << "Enter choice:";
+    cin >> choice;
+    return choice;
+}
+
 int main(){
     cout << "Initializing my project!";
+    int choice;
+    while(choice!=3){
+        choice = menu();
+        if(choice == 1){
+            cout << "\n\tEnter TEXT file location: ";
+        }else if(choice == 2){
+            cout << "\n\tEnter text: ";
+        }else if(choice == 3){
+            cout << "\n\n\n=== EXITING ===\n\n\n";
+            break;
+        }else{
+            cout << "\nPlease enter 1 / 2 / 3\n\n";
+        }
+    }
     return 0;
 }
