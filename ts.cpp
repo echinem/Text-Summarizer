7406efaf7cc167f4e51e6eaf807b834ecaeb01af
echinem <developer.himangi@gmail.com>
2025-04-21 15:32:23 +0530

    Add function to clean and tokenize text

--- ts.cpp
+++ ts.cpp
@@ -1,10 +1,91 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<vector>
+#include<sstream>
+#include<set>
 
 using namespace std;
 
+/*
+    [D]1. Split the input text into sentences using the period ('.') delimiter.
+
+    [D]2. Convert each sentence to lowercase and remove punctuation.
+
+    [D] 3. Tokenize sentences into words while filtering out stopwords.
+
+    4. Count the frequency of each meaningful word using a map.
+
+    5. Score each sentence based on the total frequency of its words.
+
+    6. Sort sentences by score and select the top 30% for the summary.
+
+    7. Reorder selected sentences to preserve their original sequence.
+
+    8. Display the final summary to the user.
+    */
+   
+set<string> stopwords = {"the", "is", "a", "an", "at", "to", "of", "and", "on"};
+
+vector<string> tokenizeAndFilter(const string& s){
+    vector<string> tokens;
+    string word;
+    stringstream ss(s);
+
+    while(ss >> word){
+        if(stopwords.find(word) == stopwords.end()){
+            tokens.push_back(word);
+        }
+    }
+    return tokens;
+}
+
+vector<string> splitToSentences(string text){
+    //split a para into individual sentences (each ending with a '.')
+    //stringstream converts string 'text' into a stream 'ss' to process it piece by piece
+    vector<string> sents;
+    stringstream ss(text);
+    string sentence;
+    
+    while(getline(ss,sentence, '.')){ // extracts substrings from ss to sentence until it sees a '.'
+        if(!sentence.empty()){
+            sentence += '.';  // re-add the period to end of sentence
+            sents.push_back(sentence);
+        }
+    }
+    return sents;
+}
+
+string cleanNormalizeText(string sentence){
+    string clean;
+    for(char ch: sentence){
+        if(isalpha(ch)||isspace(ch)){
+            clean += tolower(ch);
+        }
+    }
+    return clean;
+}
+
 //enter function to parse text
+string parseText(string text){
+    // split but append period
+    // clean: lower, remove punctuation, tokenize
+    // filter stopword
+    // 
+    
+   vector<string> sentences = splitToSentences(text);
+   
+   //clean
+   vector<vector<string>> tokens;
+
+   for(string &s: sentences){
+    s = cleanNormalizeText(s);      // returns clean lower case text
+    tokens.push_back(tokenizeAndFilter(s));     // filters out stopwords and return token words
+   }
+
+   return "Parsing complete!";
+
+}
 
 string fileText(string f){
     ifstream file(f);
@@ -19,6 +100,10 @@ string fileText(string f){
     }
     file.close();
     return text;
+}
+
+void processText(string text){
+    string parsed = parseText(text);
 }
 
 int menu(){
@@ -61,7 +146,8 @@ int main(){
         cout << "\n\n\tPress 3 to EXIT\n\tPress any other key to proceed to MENU";
         cin >> choice;
         */
-
+       processText(text);
     }
+
     return 0;
 }
